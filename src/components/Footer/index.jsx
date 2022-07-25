df21ca329d555dc47464ef0b9f6e7d4e542e8904
Edsel <pretxel100@gmail.com>
2022-07-25 14:42:29 +0100

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import Styles from './styles.module.scss';
+
+function Footer() {
+	return (
+		<footer className={Styles.footer}>
+			&copy; {new Date().getFullYear()} Edsel Serrano Montiel
+			<small className={Styles.byline}>Built by </small>
+		</footer>
+	);
+}
+export default Footer;
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,18 @@
+import { h } from 'preact';
+import Styles from './styles.module.scss';
+
+function Nav() {
+	return (
+		<nav className={Styles.nav}>
+			<a className={Styles.logolink} href="/">
+				<div className={Styles.monogram}>JW</div>
+			</a>
+			<a className={Styles.link} href="/projects">
+				Portfolio
+			</a>
+		
+			
+		</nav>
+	);
+}
+export default Nav;
