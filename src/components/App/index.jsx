942664bc4f4f9702ac42fe63c816b0dea4752965
Edsel <pretxel100@gmail.com>
2022-10-15 22:47:29 +0100

    temporal background

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,9 +5,6 @@ import { loadFull } from "tsparticles";
 const App = () => {
   
   const particlesInit = async (main) => {
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(main);
       };
     
---
+++ src/components/Body/index.jsx
@@ -0,0 +1,57 @@
+const Body = () => {
+
+    return(
+        <header id="header">
+          <h1>Edsel Serrano</h1>
+          <p>
+            Software Engineer &nbsp;&bull;&nbsp; Entrepreneur of Technology
+            &nbsp;&bull;&nbsp; Chatbot developer &nbsp;&bull;&nbsp; Machine
+            learning lover &nbsp;&bull;&nbsp; IoT developer
+          </p>
+          <nav>
+            <ul>
+              <li>
+                <a href="https://twitter.com/pretxelcom" className="fa fa-twitter"
+                  ><span>Twitter</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/serranomontiel"
+                  className="fa fa-facebook"
+                  ><span>Facebook</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/pretxel" className="fa fa-github"
+                  ><span>Github</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://mx.linkedin.com/in/edselserrano"
+                  className="fa fa-linkedin"
+                  ><span>Linkedin</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://medium.com/@edselserranomontiel"
+                  className="fa fa-medium"
+                  ><span>Medium</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:pretxel100@gmail.com?Subject=Hello"
+                  className="fa fa-envelope-o"
+                  ><span>Email</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+    )
+}
+
+export default Body;
