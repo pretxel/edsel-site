23f93bbb8ad62916cce067974a115927a6967e1c
Edsel <pretxel100@gmail.com>
2022-12-23 12:11:25 +0000

    add blog layout

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,11 +3,11 @@ import Styles from './styles.module.scss';
 function Nav() {
 	return (
 		<nav className={Styles.nav}>
-			<a className={Styles.logolink} href="/">
-				<div className={Styles.monogram}>JW</div>
-			</a>
-			<a className={Styles.link} href="/projects">
-				Portfolio
+			{/* <a className={Styles.logolink} href="/">
+				<div className={Styles.monogram}>ED</div>
+			</a> */}
+			<a className={Styles.link} href="/blog">
+				Blog
 			</a>
 		
 			
---
+++ src/pages/blog/page-1.md
@@ -0,0 +1,18 @@
+---
+layout: ../../layouts/BaseLayout.astro
+title: My Blog Post
+draft: true
+author: "Matthew Phillips"
+date: "09 Aug 2022"
+---
+
+# Hi there
+
+This Markdown file creates a page at `your-domain.com/page-1/`
+
+It probably isn't styled much, but Markdown does support:
+
+- **bold** and _italics._
+- lists
+- [links](https://astro.build)
+- and more!
