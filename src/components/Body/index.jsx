7cd51d30b3485d201405d1817416001150478b8a
Edsel <pretxel@hotmail.com>
2023-09-08 18:45:56 +0100

    fix: optim assets

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -11,39 +11,39 @@ const Body = () => {
           <nav>
             <ul>
               <li>
-                <a href="https://twitter.com/pretxelcom" className="fa fa-twitter"
+                <a href="https://twitter.com/pretxelcom" className="fa fa-twitter" aria-label="Twitter"
                   ><span>Twitter</span>
                 </a>
               </li>
               <li>
                 <a
-                  href="https://www.facebook.com/serranomontiel"
+                  href="https://www.facebook.com/serranomontiel" aria-label="Facebook"
                   className="fa fa-facebook"
                   ><span>Facebook</span>
                 </a>
               </li>
               <li>
-                <a href="https://github.com/pretxel" className="fa fa-github"
+                <a href="https://github.com/pretxel" className="fa fa-github" aria-label="Github"
                   ><span>Github</span>
                 </a>
               </li>
               <li>
                 <a
-                  href="https://mx.linkedin.com/in/edselserrano"
+                  href="https://mx.linkedin.com/in/edselserrano" aria-label="Linkedin"
                   className="fa fa-linkedin"
                   ><span>Linkedin</span>
                 </a>
               </li>
               <li>
                 <a
-                  href="https://medium.com/@edselserranomontiel"
+                  href="https://medium.com/@edselserranomontiel" aria-label="Medium"
                   className="fa fa-medium"
                   ><span>Medium</span>
                 </a>
               </li>
               <li>
                 <a
-                  href="mailto:pretxel100@gmail.com?Subject=Hello"
+                  href="mailto:pretxel100@gmail.com?Subject=Hello" aria-label="Email"
                   className="fa fa-envelope-o"
                   ><span>Email</span>
                 </a>
