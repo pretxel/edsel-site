774a34b00e8abc6c08493bb60962fc5c94d25d97
Edsel Serrano <edselserrano@Edsels-MacBook-Pro-2.local>
2023-12-26 01:33:12 -0600

    fix: update blog

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -20,7 +20,7 @@ export const posts: Post[] = [
     color: colors.teal,
     cover: "https://potatodaily.com/potato.webp",
     content:
-      "Side project que nació de la necesidad de tener una herramienta que nos permitiera realizar las Daily Scrum de manera remota.",
+      "Daily Potato es una herramienta que permite realizar las Daily Scrum de manera remota, generando el orden de participación de manera aleatoria.",
     content_p2:
       "Si deseas conocer más acerca de la Daily Scrum y cómo implementarlo en tu equipo, te recomendamos visitar la web ",
     link: "https://potatodaily.com/",
@@ -34,12 +34,25 @@ export const posts: Post[] = [
     color: colors.blue,
     cover: "/edsel-chat.png",
     content:
-      "Side project que te permite chatear conmigo, puedes preguntarme lo que quieras respecto a mi carrera profesional, proyectos, tecnologías, etc.",
+      "Edsel Chat es un chatbot que te permite chatear conmigo, puedes preguntarme lo que quieras respecto a mi carrera profesional, proyectos, tecnologías, etc.",
     content_p2: "Se hizo usando OpenAI, Langchain, React y NextJS.",
     link: "https://chat.edselserrano.com/",
     description: "Chat Edsel",
     author: "Edsel Serrano",
     date: "2023-10-01",
+  },
+  {
+    id: "3",
+    title: "Dinerqrs",
+    color: colors.gray,
+    cover: "/dineqrs.webp",
+    content:
+      "Dineqrs es una aplicación que permite a los negocios generar sus catalogos con codigos QR.",
+    content_p2: "",
+    link: "https://www.dineqrs.com/",
+    description: "Dinerqrs",
+    author: "Edsel Serrano",
+    date: "2023-12-20",
   },
 ];
 
